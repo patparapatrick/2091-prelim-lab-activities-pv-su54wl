4db222f620f772e492829a11d290ccf452a23aff
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-03-04 18:59:43 +0800

    1 file modified

--- midterms/mid-integ-form/public/server.js
+++ midterms/mid-integ-form/public/server.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/' + 'contact.html');
+  res.sendFile(__dirname + '/' + 'index.html');
 });
 
-app.get('/process_get', function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
   response = {
-    first_name: req.query.first_name,
-    last_name: req.query.last_name,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
   };
   console.log(response);
   res.end(JSON.stringify(response));
 });
-
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
