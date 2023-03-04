d532e575d312396d6e5d2e1f59c51d029f89cc10
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-03-04 20:10:26 +0800

    1 file modified, 1 deleted

--- midterms/mid-integ-form/index.js
+++ midterms/mid-integ-form/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/' + 'index.html');
+  res.sendFile(__dirname + '/' + 'contact.html');
 });
 
 const port = process.env.PORT || 3000;
