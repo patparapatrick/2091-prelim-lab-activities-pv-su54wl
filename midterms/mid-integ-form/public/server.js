fdd8ad1cd65f03b53acbea227339578e80a776b9
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-03-04 18:57:14 +0800

    1 file modified

--- midterms/mid-integ-form/public/server.js
+++ midterms/mid-integ-form/public/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/' + 'index.html');
+  res.sendFile(__dirname + '/' + 'contact.html');
 });
 
 app.get('/process_get', function (req, res) {
