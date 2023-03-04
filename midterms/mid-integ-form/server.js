c99d3f33e01813f6fcd93734fe3f4636b79a1f27
patparapatrick <123344385+patparapatrick@users.noreply.github.com>
2023-03-04 18:46:58 +0800

    1 file modified, 2 added

---
+++ midterms/mid-integ-form/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.get('/proces_get', function (req, res) {
+  respone = {
+    first_name: req.query.first_name,
+    last_name: req.query.last_name,
+  };
+
+  console.log(response);
+  res.end(JSON.stringify(response));
+});
